a30a35499ec69e4dbc4b12a34ba2ab5587f12a00
dungcnttimd <dungcnttimd@gmail.com>
2023-03-31 05:38:49 +0700

    add cBall.h class

---
+++ VSCodeDev/cBall.h
@@ -0,0 +1,20 @@
+#pragma once
+#include <windows.h>
+
+#include <d3d10.h>
+#include <d3dx10.h>
+
+#include <signal.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdarg.h>
+#include <time.h>
+#include <stdlib.h>
+
+#include <comdef.h>
+
+class cBall
+{
+
+};
+
